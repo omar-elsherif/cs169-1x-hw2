feb4107610adb947596f93d15415f348d42961d7
Omar Elsherif <omar@omarelsherif.com>
2012-11-01 04:52:22 -0700

    updated the header and footer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,74 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+'''
+def sortByName(movies)
+  newMovies = []
+  
+  while movies.length > 0
+    tempMovie = movies[0]
+    movies.delete_at(0)
+    
+    for i in (0..newMovies.length-1)
+      if tempMovie.title < newMovies[i].title
+        newMovies.insert(i, tempMovie)
+        tempMovie = nil
+        break
+      end
+    end
+    
+    if tempMovie != nil
+      newMovies << tempMovie
+    end
+  end
+  
+  return newMovies
+end
+'''
   def index
     @movies = Movie.all
+    @all_ratings = Movie.ratings
+    
+    redirect = false
+    
+    @ratings = params[:ratings]
+    
+    if @ratings == nil
+      @ratings = session[:ratings]
+      if @ratings == nil
+        @ratings = {}
+        @all_ratings.each do |rating|
+          @ratings[rating] = 1
+        end
+        session[:ratings] = @ratings
+      end
+      params[:ratings] = @ratings
+      redirect = true
+    else
+      session[:ratings] = @ratings
+    end
+    
+    @sort = params[:sort]
+    
+    if @sort == nil
+      if request.referrer != nil
+        @sort = session[:sort]
+      end
+      if @sort == nil
+        @sort = ""
+      else
+        params[:sort] = session[:sort]
+        redirect = true
+      end
+    else
+      session[:sort] = @sort
+    end
+    
+    if redirect
+      redirect_to(:action => "index", :sort => @sort, :ratings => @ratings) and return
+    end
+    
+    @movies = Movie.getByRating(@ratings.keys, @sort)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,25 @@
 class Movie < ActiveRecord::Base
+
+  def self.ratings
+    return self.find(:all, :order => "rating", :select => "rating", :conditions => "").map(&:rating).uniq
+  end
+
+  def self.getByRating(ratings, order)
+    if ratings.length > 0
+      condition = ""
+      ratings.each do |rating|
+        condition += "or rating = '#{rating}'"
+      end
+      
+      condition = condition[3..condition.length-1]
+      
+      puts condition
+      
+      #return self.all
+      return self.find(:all, :order => order, :conditions => condition)
+    else
+      #return self.find(:all, :order => "rating")
+      return self.find(:all)
+    end
+  end
 end
